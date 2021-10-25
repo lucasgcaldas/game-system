dfcdc2a104632c0056f4021f8c6b3ef49de4f46e
Lucas Gomes <lggomes007@gmail.com>
2021-10-25 11:26:15 -0300

    Adding Desert, Forest & River tests

---
+++ src/test/java/DesertTest.java
@@ -0,0 +1,43 @@
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DesertTest {
+
+    private Wizard persona;
+    //    private Warrior persona;
+    private Desert desert;
+    private Enemy enemy1, enemy2;
+    Random random = new Random();
+
+    @BeforeEach
+    @Description("If do you want to be a Warrior, comment the line 13 & 23 and uncomment the line 14, 24" +
+            "If do you want choose this equipments, uncomment the lines")
+    public void start() {
+        this.persona = new Wizard(100, 10, 5, null);
+//        this.persona = new Warrior(100, 8, 3, null);
+        this.enemy1 = new Enemy(random.nextInt((80 - 70) + 1) + 70, random.nextInt((15 - 10) + 1) + 10, random.nextInt((8 - 3) + 1) + 3);
+        this.enemy2 = new Enemy(random.nextInt((90 - 85) + 1) + 85, random.nextInt((20 - 15) + 1) + 15, random.nextInt((10 - 5) + 1) + 5);
+        this.desert = new Desert(this.persona, enemy1, enemy2);
+    }
+
+    @Test
+    @DisplayName("Should change the current scenery to forest")
+    @Description("Check if it's the same from the persona's name, because it builds a new scenery")
+    public void shouldChangeToForest() {
+        assertEquals(new Forest(persona, enemy1, enemy2).getPersona(), desert.changeToForest().getPersona());
+    }
+
+    @Test
+    @DisplayName("Should change the current scenery to river")
+    @Description("Check if it's the same from the persona's name, because it builds a new scenery")
+    public void shouldChangeToRiver() {
+        assertEquals(new River(persona, enemy1, enemy2).getPersona(), desert.changeToRiver().getPersona());
+    }
+}
